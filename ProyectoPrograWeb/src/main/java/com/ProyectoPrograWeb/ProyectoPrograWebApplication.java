3833e73c9913fc40c64a71aefa8c216281602678
Ronnny Bonilla <147563239+Ronny2115@users.noreply.github.com>
2025-06-29 21:23:10 -0600

    Add files via upload

---
+++ ProyectoPrograWeb/src/main/java/com/ProyectoPrograWeb/ProyectoPrograWebApplication.java
@@ -0,0 +1,13 @@
+package com.ProyectoPrograWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPrograWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPrograWebApplication.class, args);
+	}
+
+}
---
+++ ProyectoPrograWeb/src/test/java/com/ProyectoPrograWeb/ProyectoPrograWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoPrograWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoPrograWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
